07e12e674128c36be97a7677fde78f1d2dd93772
CALEB <juleskarebu@gmail.com>
2023-02-19 06:22:25 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - A program that prints a line with puts function
- *   *  Return: 0 (Sucess)
- *    */
+ * main - A program that prints a line with puts function
+ *  Return: 0 (Sucess)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
