2bbb4189da5cc0c16be25c569484eb66fd528e1f
CALEB <juleskarebu@gmail.com>
2023-02-27 22:23:00 -0800

    Unknow data

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
- * _isupper - Check if a letter is upper
- * @x: The number to be checked
- * Return: 1 for upper letter 0 for any else
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0.
  */
-
-int _isupper(int x)
-
+int _isupper(int c)
 {
 
-	if (x >= 65 && x <= 90)
+
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
+
 }
